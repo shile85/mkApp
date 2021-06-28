56773202b47816bb230ec27429b7f2916148a447
Vladimir Pesic <ios@vladimirpesic.rs>
2021-06-28 23:28:31 +0200

    Home page dev started

--- frontend/src/app/component/home/home.component.ts
+++ frontend/src/app/component/home/home.component.ts
@@ -6,7 +6,11 @@ import jwt_decode from 'jwt-decode';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: [
+    './home.component.css',
+    '/../../../assets/plugins/fontawesome-free/css/all.min.css',
+    '/../../../assets/dist/css/adminlte.min.css'
+  ]
 })
 export class HomeComponent implements OnInit {
 
@@ -27,8 +31,8 @@ export class HomeComponent implements OnInit {
     this.email = this.userData.email;
     this.username = this.userData.name;
     this.roleId = this.userData.roleId;
-    console.log(this.token);
-    console.log(this.userData);
+    // console.log(this.token);
+    // console.log(this.userData);
   }
 
   logout(){
