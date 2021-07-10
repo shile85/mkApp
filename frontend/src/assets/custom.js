c33f255051a692cd5deb5d9d4aafc547fb38bc78
Vladimir Pesic <ios@vladimirpesic.rs>
2021-07-10 21:04:11 +0200

    datatables plugins deleted

--- frontend/src/assets/custom.js
+++
@@ -1,7 +0,0 @@
-// $(function () {
-//     $("#example1").DataTable({
-//       "responsive": true, "lengthChange": false, "autoWidth": false,
-//       "buttons": ["copy", "csv", "excel", "pdf", "print"]
-//     }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
-//   });
-
